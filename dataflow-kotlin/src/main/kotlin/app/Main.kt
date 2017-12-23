bdc85cbf9d2dca377d723a17abed7074f4dfefdf
Koichiro Okamoto <koichirokamoto@gmail.com>
2017-12-23 22:55:47 +0900

    dataflow kotlin

---
+++ dataflow-kotlin/src/main/kotlin/app/Main.kt
@@ -0,0 +1,46 @@
+package app
+
+import org.apache.beam.sdk.Pipeline
+import org.apache.beam.sdk.io.TextIO
+import org.apache.beam.sdk.options.*
+import org.apache.beam.sdk.transforms.*
+import org.apache.beam.sdk.values.KV
+import org.apache.beam.sdk.values.TypeDescriptors
+
+interface MainOptions : PipelineOptions {
+    @Description("input file")
+    @Validation.Required
+    @Default.String("gs://apache-beam-samples/shakespeare/*")
+    fun getInputFile(): String
+
+    fun setInputFile(value: String)
+
+    @Description("output prefix")
+    @Validation.Required
+    fun getOutput(): String
+
+    fun setOutput(value: String)
+}
+
+fun main(args: Array<String>) {
+    val options = PipelineOptionsFactory.fromArgs(*args).withValidation().`as`(
+            MainOptions::class.java)
+    val p = Pipeline.create(options)
+
+    p
+            .apply("read text files", TextIO.read().from(options.getInputFile()))
+            .apply(
+                    FlatMapElements.into(TypeDescriptors.strings()).via { line ->
+                        line.split("[^\\p{L}]+")
+                    }
+            )
+            .apply("filter by word length",
+                   Filter.by(SerializableFunction<String, Boolean> { word -> !word.isEmpty() }))
+            .apply("count words", Count.perElement<String>())
+            .apply(
+                    MapElements.into(
+                            TypeDescriptors.strings()).via<KV<String, Long>> { wordCount -> wordCount.key + ": " + wordCount.value })
+            .apply("write to file", TextIO.write().to(options.getOutput()))
+
+    p.run().waitUntilFinish()
+}
