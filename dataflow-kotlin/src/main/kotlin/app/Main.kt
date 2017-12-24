7d617e3c789be554c98c41fd1a8695b236de5247
Koichiro Okamoto <koichirokamoto@gmail.com>
2017-12-24 13:23:22 +0900

    format by kotlint

--- dataflow-kotlin/src/main/kotlin/app/Main.kt
+++ dataflow-kotlin/src/main/kotlin/app/Main.kt
@@ -4,7 +4,6 @@ import org.apache.beam.sdk.Pipeline
 import org.apache.beam.sdk.io.TextIO
 import org.apache.beam.sdk.options.*
 import org.apache.beam.sdk.transforms.*
-import org.apache.beam.sdk.values.KV
 import org.apache.beam.sdk.values.TypeDescriptors
 
 interface MainOptions : PipelineOptions {
@@ -23,24 +22,16 @@ interface MainOptions : PipelineOptions {
 }
 
 fun main(args: Array<String>) {
-    val options = PipelineOptionsFactory.fromArgs(*args).withValidation().`as`(
-            MainOptions::class.java)
+    val options = PipelineOptionsFactory.fromArgs(*args).withValidation().`as`(MainOptions::class.java)
     val p = Pipeline.create(options)
 
     p
-            .apply("read text files", TextIO.read().from(options.getInputFile()))
-            .apply(
-                    FlatMapElements.into(TypeDescriptors.strings()).via { line ->
-                        line.split("[^\\p{L}]+")
-                    }
-            )
-            .apply("filter by word length",
-                   Filter.by(SerializableFunction<String, Boolean> { word -> !word.isEmpty() }))
-            .apply("count words", Count.perElement<String>())
-            .apply(
-                    MapElements.into(
-                            TypeDescriptors.strings()).via<KV<String, Long>> { wordCount -> wordCount.key + ": " + wordCount.value })
-            .apply("write to file", TextIO.write().to(options.getOutput()))
+        .apply("read text files", TextIO.read().from(options.getInputFile()))
+        .apply(FlatMapElements.into(TypeDescriptors.strings()).via { line -> line.split("[^\\p{L}]+") })
+        .apply("filter by word length", Filter.by(SerializableFunction { word: String -> !word.isEmpty() }))
+        .apply("count words", Count.perElement<String>())
+        .apply(MapElements.into(TypeDescriptors.strings()).via { wordCount -> wordCount.key + ": " + wordCount.value })
+        .apply("write to file", TextIO.write().to(options.getOutput()))
 
     p.run().waitUntilFinish()
 }
